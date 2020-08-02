1d57a305b6b2107e2b0c85ff1724dce5df38e828
Simo Aleksandrov <simo3003@me.com>
2020-08-02 20:24:14 +0300

    string method on player type

--- game.go
+++ game.go
@@ -1,35 +1,42 @@
 package main
 
+type Player int
+
+const (
+	// playerNil is the default value
+	playerNil Player = iota
+	PlayerX
+	PlayerO
+)
+
 // Game represents a game of TicTacToe.
+// `PlayerO` goes first.
 type Game struct {
-	State     Board
-	TurnCount int
+	State     [3][3]Player
+	turnCount int
 }
 
 // NewGame is a game constructor.
 func NewGame() *Game {
 	return &Game{
-		State:     NewBoard(),
-		TurnCount: 0,
+		State: [3][3]Player{
+			{playerNil, playerNil, playerNil},
+			{playerNil, playerNil, playerNil},
+			{playerNil, playerNil, playerNil},
+		},
+		turnCount: 0,
 	}
 }
 
-// Board is a game board.
-// 0 means Blank.
-// 1 means X.
-// 2 means O.
-type Board [3][3]int
-
-// NewBoard is a constructor for a board.
-func NewBoard() Board {
-	return Board{
-		{0, 0, 0},
-		{0, 0, 0},
-		{0, 0, 0},
+// Put places the value on the coordinates.
+func (g *Game) Put(i, j int) {
+	switch g.turnCount % 2 {
+	case 0:
+		g.State[i][j] = PlayerO
+	case 1:
+		g.State[i][j] = PlayerX
+	default:
+		g.State[i][j] = playerNil
 	}
-}
-
-// Put places the value on the coordinates
-func (b Board) Put(i, j int, val int) {
-	b[i][j] = val
+	g.turnCount++
 }
